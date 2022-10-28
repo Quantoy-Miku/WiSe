6fe71cc2432e778b851df741d2bf3a3ca9d8ec1d
Quantoy-Miku <52102518+Quantoy-Miku@users.noreply.github.com>
2022-10-28 16:57:00 +0200

    Create Quersumme.java

---
+++ Übungsblätter/src/Praktika/Praktikum_1/Quersumme.java
@@ -0,0 +1,10 @@
+package Praktika.Praktikum_1;
+
+public class Quersumme {
+    public static void main(String[] args) 
+    {
+        System.out.println(digitSum(345)); 
+    }
+    public static int digitSum( int x )
+    return digitSum(x);
+}
