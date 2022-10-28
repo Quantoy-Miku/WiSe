7502f35626997b8979cc0e472f1487ced17d8962
Quantoy-Miku <52102518+Quantoy-Miku@users.noreply.github.com>
2022-10-28 18:21:12 +0200

    Quersumme Fix + Text

--- Übungsblätter/src/Praktika/Praktikum_1/Quersumme.java
+++ Übungsblätter/src/Praktika/Praktikum_1/Quersumme.java
@@ -4,14 +4,16 @@ public class Quersumme
 {
     public static void main(String[] args) 
     {
-        System.out.println(digitSum(341)); 
+        System.out.println(digitSum(391)); 
     }
     public static int digitSum( int x )
     {
-        int sum=0;
+        int sum = 0;
         while( x != 0)
-        sum = sum + ( x % 10);
-        x = sum / 10;
+    {
+         sum = sum + (x % 10);
+        x = x / 10;
     }
-    return sum;
+        return sum;
+    }
 }
--- Übungsblätter/src/Praktika/Praktikum_1/Text.java
+++ Übungsblätter/src/Praktika/Praktikum_1/Text.java
@@ -1,5 +1,14 @@
 package Praktika.Praktikum_1;
 
-public class Text {
-    
+public class Text 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println(digitToString(5));
+    }
+        public static String[] zahlen={"null","eins","zwei","drei","vier","fünf","sechs","sieben","acht","neun"};
+        public static String digitToString( int z1 )
+        {
+        return zahlen[z1];
+        }
 }
